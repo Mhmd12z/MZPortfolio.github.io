58c6e81c5bce12182a926bf0893ff634ad26c1cc
Mhmd12z <98625548+Mhmd12z@users.noreply.github.com>
2023-08-13 13:34:23 +0300

    script.js

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ menuBtn.onclick = function () {
   navLinks.classList.toggle("active");
   navSearch.classList.toggle("active");
 };
+let logo=document.querySelector(".logo");
+logo.addEventListener("click",function(){
+  scrollTo(0,0);
+})
 // let links=document.querySelectorAll(".nav .links a");
-// let home=document.querySelector(".main");
+
 // let aboutSec=document.querySelector(".about");
 // let portfolioSec=document.querySelector(".projects");
 // let contactSec=document.querySelector(".contact");
@@ -23,3 +27,41 @@ menuBtn.onclick = function () {
 //         links[1].classList.add("active-link");
 //     }
 // }
+let iconReturn=document.createElement("i");
+iconReturn.classList.add("fa-solid");
+iconReturn.classList.add("fa-arrow-up");
+iconReturn.classList.add("fa-bounce");
+let returnBtn=document.createElement("button");
+returnBtn.append(iconReturn);
+
+returnBtn.style.cssText=`
+position: fixed;
+bottom: 20px;
+right: 20px;
+font-size: 40px;
+background-color: #2e3135;
+border: none;
+color: var(--main-color);
+padding: 20px 10px;
+border-radius: 5px;
+cursor: pointer;
+z-index: -1;
+opacity: 0;
+transition: var(--transition);
+`;
+returnBtn.addEventListener("click",function(){
+  document.body.scrollTop=0;
+  document.documentElement.scrollTop=0;
+})
+document.body.appendChild(returnBtn);
+let home=document.querySelector(".main");
+window.onscroll=function(){
+  if(scrollY>home.offsetTop + 600){
+    returnBtn.style.zIndex=100;
+    returnBtn.style.opacity=1;
+  }
+  else{
+    returnBtn.style.zIndex=-1;
+    returnBtn.style.opacity=0;
+  }
+}
