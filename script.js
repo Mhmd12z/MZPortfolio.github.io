30fd2507e9113acb21add2249e6163a1ea9b3cdd
Mhmd12z <98625548+Mhmd12z@users.noreply.github.com>
2023-08-16 14:06:11 +0300

    script.js

--- script.js
+++ script.js
@@ -5,36 +5,18 @@ menuBtn.onclick = function () {
   navLinks.classList.toggle("active");
   navSearch.classList.toggle("active");
 };
-let logo=document.querySelector(".logo");
-logo.addEventListener("click",function(){
-  scrollTo(0,0);
-})
-// let links=document.querySelectorAll(".nav .links a");
-
-// let aboutSec=document.querySelector(".about");
-// let portfolioSec=document.querySelector(".projects");
-// let contactSec=document.querySelector(".contact");
-// window.onscroll=function(){
-//     if(scrollY<document.documentElement.clientHeight){
-//         links[0].className="";
-//         links[1].className="";
-//     }
-//     if(scrollY>aboutSec.offsetTop - 100 && scrollY<portfolioSec.offsetTop){
-//         links[0].classList.add("active-link");
-//     }
-//     else if(scrollY>portfolioSec.offsetTop){
-//         links[0].classList.remove("active-link");
-//         links[1].classList.add("active-link");
-//     }
-// }
-let iconReturn=document.createElement("i");
+let logo = document.querySelector(".logo");
+logo.addEventListener("click", function () {
+  scrollTo(0, 0);
+});
+let iconReturn = document.createElement("i");
 iconReturn.classList.add("fa-solid");
 iconReturn.classList.add("fa-arrow-up");
 iconReturn.classList.add("fa-bounce");
-let returnBtn=document.createElement("button");
+let returnBtn = document.createElement("button");
 returnBtn.append(iconReturn);
 
-returnBtn.style.cssText=`
+returnBtn.style.cssText = `
 position: fixed;
 bottom: 20px;
 right: 20px;
@@ -49,25 +31,42 @@ z-index: -1;
 opacity: 0;
 transition: var(--transition);
 `;
-returnBtn.addEventListener("click",function(){
-  document.body.scrollTop=0;
-  document.documentElement.scrollTop=0;
-})
+returnBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
 document.body.appendChild(returnBtn);
-let home=document.querySelector(".main");
-window.onscroll=function(){
-  if(scrollY>home.offsetTop + 600){
-    returnBtn.style.zIndex=100;
-    returnBtn.style.opacity=1;
+let home = document.querySelector(".main");
+window.onscroll = function () {
+  if (scrollY > home.offsetTop + 600) {
+    returnBtn.style.zIndex = 100;
+    returnBtn.style.opacity = 1;
+  } else {
+    returnBtn.style.zIndex = -1;
+    returnBtn.style.opacity = 0;
   }
-  else{
-    returnBtn.style.zIndex=-1;
-    returnBtn.style.opacity=0;
-  }
-}
-let twitter=document.querySelectorAll(".x-twitter");
-for(let i=0;i<twitter.length;i++){
-  twitter[i].addEventListener("click",function(){
-    window.location.href='https://twitter.com/m7mdzeidan';
+};
+let twitter = document.querySelectorAll(".x-twitter");
+for (let i = 0; i < twitter.length; i++) {
+  twitter[i].addEventListener("click", function () {
+    window.location.href = "https://twitter.com/m7mdzeidan";
   });
 }
+let link = document.querySelectorAll(".links a");
+let navMenu = document.querySelector(".nav .active");
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+}
+let card = document.querySelectorAll(".projects .cards .more");
+let viewBtn = document.querySelector(".view-more");
+viewBtn.onclick = function () {
+  for (let i = 0; i < card.length; i++) {
+    card[i].classList.toggle("close");
+  }
+  if (card[0].classList.contains("close")) viewBtn.innerHTML = "View More";
+  else {
+    viewBtn.innerHTML = "View Less";
+  }
+};
